5ae46208112f88fbe360b1bde9d320c701c440ee
App Academy Student <student@appacademy.io>
2014-02-19 17:25:58 -0800

    Refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rentcats::Application.routes.draw do
   root to: 'cats#index'
 
   resources :cats do
-    resources :cat_rental_requests
+    resources :cat_rental_requests, only: [:create, :update]
   end
 
-  post '/cats/:cat_id/cat_rental_requests/:id', to: 'cat_rental_requests#approve', as: 'approve'
-  post '/cats/:cat_id/cat_rental_requests/:id', to: 'cat_rental_requests#deny', as: 'deny'
+  post '/cats/:cat_id/cat_rental_requests/:id/approve', to: 'cat_rental_requests#approve', as: 'approve'
+  post '/cats/:cat_id/cat_rental_requests/:id/deny', to: 'cat_rental_requests#deny', as: 'deny'
 
 end
